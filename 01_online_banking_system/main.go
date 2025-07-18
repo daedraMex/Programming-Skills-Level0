9ca228026d3cd27914f6370c8b67c383412a258a
Erika Flores <foglzerika@gmail.com>
2025-07-17 22:13:27 -0600

    feat | create condition for bucle

--- 01_online_banking_system/main.go
+++ 01_online_banking_system/main.go
@@ -6,28 +6,30 @@ import (
 )
 
 const (
-	Attemps  = 2
-	Username = "wonder"
-	Password = "woman"
+	validAttemps = 2
+	Username     = "wonder"
+	Password     = "woman"
 )
 
 func main() {
 	fmt.Println("WELCOME")
-
+	//var declaration
 	inputUser, inputPassword := "", ""
-	fmt.Print("Insert user : ")
-	fmt.Scanln(&inputUser)
-
-	fmt.Print("Insert password: ")
-	fmt.Scanln(&inputPassword)
-
-	lowerUser := strings.ToLower(inputUser)
-	lowerPassword := strings.ToLower(inputPassword)
+	//attemps := 0
 
 	fmt.Println(inputUser, inputPassword)
-	if lowerUser == Username && lowerPassword == Password {
-		fmt.Println("Welcome to the Manchester United Bank")
-	} else {
-		fmt.Println("Good look for the next time")
+	for attemps := 0; attemps <= validAttemps; attemps++ {
+		fmt.Print("Insert user : ")
+		fmt.Scanln(&inputUser)
+
+		fmt.Print("Insert password: ")
+		fmt.Scanln(&inputPassword)
+
+		lowerUser := strings.ToLower(inputUser)
+		lowerPassword := strings.ToLower(inputPassword)
+		if lowerUser == Username && lowerPassword == Password {
+			fmt.Println("Welcome to the Manchester United Bank")
+		}
 	}
+	fmt.Println("Good look for the next one")
 }
