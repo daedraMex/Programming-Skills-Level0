46a159204b9f0b386e3ebf543fa59d1752e25e9b
Erika Flores <foglzerika@gmail.com>
2025-07-17 21:55:03 -0600

    feat | create validations user and pass

---
+++ 01_online_banking_system/go.mod
@@ -0,0 +1,3 @@
+module onlineBanking
+
+go 1.24.5
--- 01_online_banking_system/main.go
+++ 01_online_banking_system/main.go
@@ -1,14 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	Attemps  = 2
-	Username = "Wonder"
+	Username = "wonder"
 	Password = "woman"
 )
 
 func main() {
 	fmt.Println("WELCOME")
 
+	inputUser, inputPassword := "", ""
+	fmt.Print("Insert user : ")
+	fmt.Scanln(&inputUser)
+
+	fmt.Print("Insert password: ")
+	fmt.Scanln(&inputPassword)
+
+	lowerUser := strings.ToLower(inputUser)
+	lowerPassword := strings.ToLower(inputPassword)
+
+	fmt.Println(inputUser, inputPassword)
+	if lowerUser == Username && lowerPassword == Password {
+		fmt.Println("Welcome to the Manchester United Bank")
+	} else {
+		fmt.Println("Good look for the next time")
+	}
 }
